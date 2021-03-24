347abda7572d45ae21a74d2b8e06708c3b36bb10
sflittrell <sflittrell@gmail.com>
2021-03-24 17:37:42 -0400

    complete button changes state of item and close button deletes item

--- src/App.js
+++ src/App.js
@@ -7,41 +7,67 @@ class App extends React.Component {
 
   constructor() {
     super();
-    this.test = 'test';
     this.state = {
       taskArr: [],
       taskInput: '',
-      completed: false
     }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.closeTask = this.closeTask.bind(this);
+    this.completed = this.completed.bind(this);
   }
 
   handleChange(e) {
     this.setState({ taskInput: e.target.value })
-    console.log(this.state.taskInput)
+    // console.log(this.state.taskInput)
   }
   handleSubmit(e) {
     if (this.state.taskInput !== '') {
-      console.log(this.state.taskInput)
+      let newTask = {
+        id: Date.now(),
+        taskText: this.state.taskInput,
+        completed: false
+      }
+      // console.log(this.state.taskInput)
       this.setState({
-        taskArr: [...this.state.taskArr, this.state.taskInput],
-        taskInput: ''
+        taskArr: [...this.state.taskArr, newTask],
+        taskInput: '',
       })
     }
     e.preventDefault();
   }
 
-  renderedTasks(item, index) {
-    return <Task taskInput={item} key={index} />
+  completed(id) {
+    this.setState({
+      taskArr: this.state.taskArr.map(newTask => {
+        if (newTask.id === id) {
+          newTask.completed = !newTask.completed
+        }
+        return newTask
+      })
+    })
   }
+
+  closeTask(id) {
+    // function filterHelper(task) {
+    //   if (task.id !== id) {
+    //     return task
+    //   }
+    // }
+    const filteredTasks = this.state.taskArr.filter(task => task.id !== id);
+
+    // console.log(filteredTasks);
+    // console.log(this.state.taskArr)
+    this.setState({ taskArr: filteredTasks });
+    console.log(this.state.taskArr)
+
+  }
+
 
   itemsLeft() {
     return this.state.taskArr.length;
   }
-
-
 
   render() {
 
@@ -58,17 +84,28 @@ class App extends React.Component {
               <div className="input-group mb-3">
                 <input
                   type="text"
+                  value={this.state.taskInput}
                   className="form-control"
                   placeholder="What needs to be done?"
                   onChange={this.handleChange}
                 />
-                {/* <input className="btn btn-outline-secondary" type="submit" value="Submit" /> */}
-                <button className="btn btn-outline-secondary" type="button" id="subBtn" onClick={this.handleSubmit}>Submit</button>
+                <button
+                  className="btn btn-outline-secondary"
+                  // value={this.state.taskInput}
+                  type="button" id="subBtn"
+                  onClick={this.handleSubmit}>
+                  Submit
+                </button>
               </div>
             </div>
             <div className="row">
               <ul className="tasks list-unstyled">
-                {this.state.taskArr.map(this.renderedTasks)}
+                {this.state.taskArr.map((item, index) => <Task
+                  newTask={item}
+                  key={index}
+                  closeTask={this.closeTask}
+                  completed={this.completed}
+                />)}
 
               </ul>
             </div>
--- src/Task.js
+++ src/Task.js
@@ -1,33 +1,48 @@
-import { render } from '@testing-library/react';
-import React from 'react';
-
 function Task(props) {
 
-    // console.log(props)
 
-        return (
-            <li>
+    //    function checkBtn() {
+    //         if (props.newTask.completed) {
+    //             return <i className="bi bi-check-circle-fill"></i>;
+    //         } else {
+    //             return <i className="bi bi-circle"></i>
+    //         }
+    //     }
+
+    function deleteItem() {
+        props.closeTask(props.newTask.id);
+    }
+
+    function setCompleted() {
+        props.completed(props.newTask.id);
+    }
+
+    return (
+        <li>
             <div className="task text-start">
                 <div className="form-check border p-3">
-                    <input 
-                    className="form-check-input ms-0" 
-                    type="checkbox" 
-                    value="" 
-                    />
-                    <label 
-                    className="form-check-label ms-2" 
-                    htmlFor="flexCheckDefault">
-                        {props.taskInput}
-                    </label>
-                    <button 
-                    type="button" 
-                    className="btn-close btn-sm position-absolute end-0 me-4">
+                    {/* {checkBtn()} */}
+                    <button
+                        type="button"
+                        className="btn btn-sm btn-outline-secondary"
+                        onClick={setCompleted}
+                    >
+                        Completed
+                    </button>
+                    <span className={props.newTask.completed ? "text-decoration-line-through ms-2" : "ms-2"}>
+                        {props.newTask.taskText}</span>
+                    <button
+                        type="button"
+                        className="btn-close btn-sm position-absolute end-0 me-4"
+                        onClick={deleteItem}
+                    >
                     </button>
                 </div>
             </div>
-            </li>
-        )
-    }
+        </li>
+    )
+}
+
 
 
 export default Task;
